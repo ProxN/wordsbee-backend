5707045c9cd9689a2c0b88447fee45b5d87474d1
proxn <idel.ayoubkan@gmail.com>
2020-08-22 14:31:37 +0100

    Add sameSite attr

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -28,6 +28,7 @@ export const sendToken = (obj: IObj): void => {
     expires: new Date(Date.now() + cookiesExpires * 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+    sameSite: 'strict',
   });
 
   user.password = undefined;
