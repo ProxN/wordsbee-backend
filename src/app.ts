410e87196785bdb4a621ec382ae7d7375568ac75
proxn <idel.ayoubkan@gmail.com>
2020-08-22 14:27:29 +0100

    Allow access from the frontend

--- src/app.ts
+++ src/app.ts
@@ -17,34 +17,32 @@ const app: Application = express();
 /**
  * Middlewares
  */
-app.set('trust proxy', 1);
+app.enable('trust proxy');
 
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://wordsbee.netlify.app',
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          'The CORS policy for this site does not ' +
+          'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
 
 app.options('*', cors());
-
-// const allowedOrigins = [
-//   'http://localhost:3000',
-//   'https://wordsbee.netlify.app',
-// ];
-
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       // allow requests with no origin
-//       // (like mobile apps or curl requests)
-//       if (!origin) return callback(null, true);
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         const msg =
-//           'The CORS policy for this site does not ' +
-//           'allow access from the specified Origin.';
-//         return callback(new Error(msg), false);
-//       }
-//       return callback(null, true);
-//     },
-//     credentials: true,
-//   })
-// );
 
 app.use(express.json());
 
